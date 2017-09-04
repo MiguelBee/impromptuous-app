04ec2796203b926b29637ef16dc8fc41d7dab54e
Miguel Bustamante <mbustamante98@hotmail.com>
2017-09-03 21:18:39 -0600

    Add logic to controller for random selections and completed show pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,30 @@
 class OrdersController < ApplicationController
 
 	def index
-		@order = Order.all
+		@order = Order.new
 	end
 
 	def create_delivery
 		@order = Order.create(delivery_params)
-		redirect_to order_path(@order)
+		redirect_to delivery_path(@order)
 	end
 
 	def create_eat_out
 		@order = Order.create(eat_out_params)
-		#choice = results.sort_by("RANDOM()").first
-		redirect_to order_path(@order)
+		redirect_to eat_out_path(@order)
 	end
 
-	def show
-		@order = Order.find(params[:id])
-		if @order.radius.present?
-			radius_in_kilometers = (@order.radius*160).to_i
-			logger.info "this radius in kilometers is #{radius_in_kilometers}"
-			@results = YelpApiService.business_search(@order.zipcode, radius_in_kilometers)
-		else
-			@results = YelpApiService.delivery_search(@order.address1, @order.city, @order.state)
-		end
+	def delivery
+		order = Order.find(params[:id])
+		results = YelpApiService.delivery_search(order.address1, order.city, order.state)
+		@random_choice = results['businesses'].to_a.shuffle.pop
+	end
+
+	def eat_out
+		order = Order.find(params[:id])
+		radius_in_kilometers = (order.radius*160).to_i
+		results = YelpApiService.business_search(order.zipcode, radius_in_kilometers)
+		@random_choice = results['businesses'].to_a.shuffle.pop
 	end
 
 	private
